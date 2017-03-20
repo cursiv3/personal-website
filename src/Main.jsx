9da2fbe725e970918c9bd847ce12f6328c8b2dfd
Corey <lewisc503@gmail.com>
2017-03-20 15:51:56 -0700

    initial commit

---
+++ src/Main.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Main.css';
+
+class Main extends Component {
+
+
+  render() {
+    return (
+      <div className="App">
+       
+      </div>
+    );
+  }
+}
+
+export default Main;
