9d3e1f1a74138edab0656bc92a0783db1c8c8ccb
Corey <lewisc503@gmail.com>
2017-04-22 20:08:53 -0700

    resume page row added

--- src/components/ResumePage.jsx
+++ src/components/ResumePage.jsx
@@ -21,19 +21,26 @@ class ResumePage extends Component {
     render() {
         return (
             <div ref={s => this.section = s} className="sectionContainer">
-                <a href="/static/media/CoreyLewisResumeVisual.5f7c2f42.jpg" target="_blank">
-                    <img src={require("../../public/CoreyLewisResumeVisual.jpg")} className="img-responsive" alt="Corey Lewis Visual Resume" />
-                </a>
-                <a download="CoreyLewisResumeVisual.5f7c2f42.jpg" href="/static/media/CoreyLewisResumeVisual.5f7c2f42.jpg">
-                    <button className="resumeBtn">download</button>
-                    <hr/>
-                </a>
-                <a href="https://drive.google.com/open?id=0Bw92SM4ozWAJbUZmT2E2dUM3SmM" target="_blank">
-                    <img src={require("../../public/CoreyLewisResumeTyped.jpg")} className="img-responsive" />
-                </a>
-                <a href="https://drive.google.com/uc?export=download&id=0Bw92SM4ozWAJbUZmT2E2dUM3SmM">
-                    <button className="resumeBtn">download</button>
-                </a>
+                <Row>
+                    <Col sm={6}>
+                        <h3 className="text-center">Visual Resume</h3>
+                        <a href="/static/media/CoreyLewisResumeVisual.5f7c2f42.jpg" target="_blank" classID>
+                            <img src={require("../../public/CoreyLewisResumeVisual.jpg")} alt="Corey Lewis Visual Resume" className="img-responsive resumeImg" />
+                        </a>
+                        <a download="CoreyLewisResumeVisual.5f7c2f42.jpg" href="/static/media/CoreyLewisResumeVisual.5f7c2f42.jpg">
+                            <button className="resumeBtn">download</button>
+                        </a>
+                    </Col>
+                    <Col sm={6}>
+                        <h3 className="text-center">Text Resume</h3>
+                        <a href="https://drive.google.com/open?id=0Bw92SM4ozWAJbUZmT2E2dUM3SmM" target="_blank">
+                            <img src={require("../../public/CoreyLewisResumeTyped.jpg")} className="img-responsive resumeImg" />
+                        </a>
+                        <a href="https://drive.google.com/uc?export=download&id=0Bw92SM4ozWAJbUZmT2E2dUM3SmM">
+                            <button className="resumeBtn">download</button>
+                        </a>
+                    </Col>
+                </Row>
             </div>
         )
     }
