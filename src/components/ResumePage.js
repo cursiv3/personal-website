dc7283627e81d4a5bc4b5d8a1613cedf99637b75
Corey <lewisc503@gmail.com>
2017-06-29 21:18:51 -0700

    Resume and porfolio updates

--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -1,49 +1,78 @@
-import React, {Component} from 'react'
-import '../styles/main.css'
-import {Row, Col} from 'react-bootstrap'
-import TweenMax from 'gsap'
+import React, { Component } from "react";
+import "../styles/main.css";
+import { Row, Col } from "react-bootstrap";
+import TweenMax from "gsap";
 
 class ResumePage extends Component {
-    componentWillMount () {
-        setTimeout(() => {window.scrollTo(0, 0)}, 500);
-    }
-    
-    componentWillEnter (callback) {
-        const box = this.section;
-        TweenMax.fromTo(box, 0.5, { opacity: 0 }, { delay: 0.5, opacity: 1, onComplete: callback })
-    }
+  componentWillMount() {
+    setTimeout(() => {
+      window.scrollTo(0, 0);
+    }, 500);
+  }
 
-    componentWillLeave (callback) {
-        const box = this.section;
-        TweenMax.fromTo(box, 0.5, { opacity: 1 }, { opacity: 0, onComplete: callback });
-    }
+  componentWillEnter(callback) {
+    const box = this.section;
+    TweenMax.fromTo(
+      box,
+      0.5,
+      { opacity: 0 },
+      { delay: 0.5, opacity: 1, onComplete: callback }
+    );
+  }
 
-    render() {
-        return (
-            <div ref={s => this.section = s} className="sectionContainer">
-                <Row>
-                    <Col sm={6}>
-                        <h3 className="text-center">Visual Resume</h3>
-                        <a href="/static/media/CoreyLewisResumeVisual.c027a96c.png" target="_blank" classID>
-                            <img src={require("../../public/CoreyLewisResumeVisual.png")} alt="Corey Lewis Visual Resume" className="img-responsive resumeImg" />
-                        </a>
-                        <a download="CoreyLewisResumeVisual.c027a96c.png" href="/static/media/CoreyLewisResumeVisual.c027a96c.png">
-                            <button className="resumeBtn">download</button>
-                        </a>
-                    </Col>
-                    <Col sm={6}>
-                        <h3 className="text-center">Text Resume</h3>
-                        <a href="https://drive.google.com/open?id=0Bw92SM4ozWAJbUZmT2E2dUM3SmM" target="_blank">
-                            <img src={require("../../public/CoreyLewisResumeTyped.jpg")} className="img-responsive resumeImg" />
-                        </a>
-                        <a href="https://drive.google.com/uc?export=download&id=0Bw92SM4ozWAJbUZmT2E2dUM3SmM">
-                            <button className="resumeBtn">download</button>
-                        </a>
-                    </Col>
-                </Row>
-            </div>
-        )
-    }
+  componentWillLeave(callback) {
+    const box = this.section;
+    TweenMax.fromTo(
+      box,
+      0.5,
+      { opacity: 1 },
+      { opacity: 0, onComplete: callback }
+    );
+  }
+
+  render() {
+    return (
+      <div ref={s => (this.section = s)} className="sectionContainer">
+        <Row>
+          <Col sm={6}>
+            <h3 className="text-center">Visual Resume</h3>
+            <a
+              href="/static/media/CoreyLewisResumeVisual.e1e7d5a4.png"
+              target="_blank"
+              classID
+            >
+              <img
+                src={require("../../public/CoreyLewisResumeVisual.png")}
+                alt="Corey Lewis Visual Resume"
+                className="img-responsive resumeImg"
+              />
+            </a>
+            <a
+              download="CoreyLewisResumeVisual.e1e7d5a4.png"
+              href="/static/media/CoreyLewisResumeVisual.e1e7d5a4.png"
+            >
+              <button className="resumeBtn">download</button>
+            </a>
+          </Col>
+          <Col sm={6}>
+            <h3 className="text-center">Text Resume</h3>
+            <a
+              href="https://drive.google.com/file/d/0Bw92SM4ozWAJLTRDeGJyX1ZQcjQ/view?usp=sharing"
+              target="_blank"
+            >
+              <img
+                src={require("../../public/CoreyLewisResumeTyped.jpg")}
+                className="img-responsive resumeImg"
+              />
+            </a>
+            <a href="https://drive.google.com/uc?export=download&id=0Bw92SM4ozWAJLTRDeGJyX1ZQcjQ">
+              <button className="resumeBtn">download</button>
+            </a>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default ResumePage;
--- src/components/stateFile.js
+++ src/components/stateFile.js
@@ -1,87 +1,92 @@
 module.exports = {
-    currentDisplay: 'landing',
-    navBar: 'navEntry',
-    about: {
-        media: require('../../public/coreyPort.jpg'),
-        media2: require('../../public/coreysoccer.jpg'),
-        media3: require('../../public/coreychewie.jpg'),
-        media4: require('../../public/corey2.jpg'),
+  currentDisplay: "landing",
+  navBar: "navEntry",
+  about: {
+    media: require("../../public/coreyPort.jpg"),
+    media2: require("../../public/coreysoccer.jpg"),
+    media3: require("../../public/coreychewie.jpg"),
+    media4: require("../../public/corey2.jpg")
+  },
+  navBtn: [
+    "navBtn navHov",
+    "navBtn navHov",
+    "navBtn navHov",
+    "navBtn navHov",
+    "navBtn navHov"
+  ],
+  portfolio: [
+    {
+      id: 8,
+      link: "https://codepen.io/cursiv3/pen/GERqGP",
+      img: require("../../public/calc.png"),
+      text: `This was a freecodecamp.com front end challenge.`
     },
-    navBtn: [
-        'navBtn navHov',
-        'navBtn navHov',
-        'navBtn navHov',
-        'navBtn navHov',
-        'navBtn navHov',
-    ],
-    portfolio: [
-        {
-                id: 7, 
-                link: 'https://codepen.io/cursiv3/pen/dWGqYE',
-                img: require('../../public/locationPage.jpg'),
-                text: `This was a solo project I was given at the end of my
+    {
+      id: 7,
+      link: "https://codepen.io/cursiv3/pen/dWGqYE",
+      img: require("../../public/locationPage.jpg"),
+      text: `This was a solo project I was given at the end of my
                 internship with Transflection. It was a mock up for a detail
-                view that could be used for the different client locations.`,
-            },
-            {
-                id: 6, 
-                link: 'http://codepen.io/cursiv3/pen/qqMMpv/',
-                img: require('../../public/twitch.jpg'),
-                text: `In this challenge I learned to query the Twitch.tv 
+                view that could be used for the different client locations.`
+    },
+    {
+      id: 6,
+      link: "http://codepen.io/cursiv3/pen/qqMMpv/",
+      img: require("../../public/twitch.jpg"),
+      text: `In this challenge I learned to query the Twitch.tv 
                 API, show if a channel is online, and if so what they're 
-                streaming, or a placeholder if the user has closed their 
-                account or it doesn't exist.`,
-            },
-            { 
-                id: 5,
-                link: 'http://codepen.io/cursiv3/pen/PbKPEv',
-                img: require('../../public/wiki.jpg'),
-                text: `In this challenge I simply needed to query 
+                streaming.`
+    },
+    {
+      id: 5,
+      link: "http://codepen.io/cursiv3/pen/PbKPEv",
+      img: require("../../public/wiki.jpg"),
+      text: `In this challenge I simply needed to query 
                 the wikipedia API based on the string typed into the search 
                 bar and display matches. Their API is not 
-                very intuitive.`,
-            },
-            { 
-                id: 4,
-                link: 'http://codepen.io/cursiv3/pen/ozdrzG',
-                img: require('../../public/weather.jpg'),
-                text: `In this challenge I learned how to grab geolocation data
-                using an IP address, then get weather data from a weather API.`,
-            },
-            { 
-                id: 3,
-                link: 'http://codepen.io/cursiv3/pen/qaVQqv',
-                img: require('../../public/quote.jpg'),
-                text: `This was my first time working with an API. It's a simple 
-                random quote generator with a button to share the quote to twitter.`,
-            },
-            { 
-                id: 2,
-                link: 'http://codepen.io/cursiv3/pen/NRGZPV',
-                img: require('../../public/personalsite.jpg'),
-                text: `This was my first time working with bootstrap. The 
-                challenge was to build a very simple personal website.`,
-            },
-            { 
-                id: 1,
-                link: 'http://codepen.io/cursiv3/pen/pEJmLK',
-                img: require('../../public/curie.jpg'),
-                text: `This was the first front end challege presented by 
+                very intuitive.`
+    },
+    {
+      id: 4,
+      link: "http://codepen.io/cursiv3/pen/ozdrzG",
+      img: require("../../public/weather.jpg"),
+      text: `In this challenge I learned how to grab geolocation data
+                using an IP address, then get weather data from a weather API.`
+    },
+    {
+      id: 3,
+      link: "http://codepen.io/cursiv3/pen/qaVQqv",
+      img: require("../../public/quote.jpg"),
+      text: `This was my first time working with an API. It's a simple 
+                random quote generator with a button to share the quote to twitter.`
+    },
+    {
+      id: 2,
+      link: "http://codepen.io/cursiv3/pen/NRGZPV",
+      img: require("../../public/personalsite.jpg"),
+      text: `This was my first time working with bootstrap. The 
+                challenge was to build a very simple personal website.`
+    },
+    {
+      id: 1,
+      link: "http://codepen.io/cursiv3/pen/pEJmLK",
+      img: require("../../public/curie.jpg"),
+      text: `This was the first front end challege presented by 
                 freecodecamp.com.  This was also the first front end page I ever
-                put together.`,
-            },
-        ],
-    portfolioIntro: `I began to work through the curriculum at freecodecamp.com 
+                put together.`
+    }
+  ],
+  portfolioIntro: `I began to work through the curriculum at freecodecamp.com 
                 in July of 2016. Each section was instruction and algorithm challenges 
                 to get you thinking about complex tasks using the different tools available 
                 in JavaScript. These challenges were the culmination of each section.  I've 
                 linked these to show more where I came from rather than what I can do. The
                 code for this site is `,
-    music: [],
-    contact: {
-        media: '',
-        text: [],
-    },
-    visResume: '../../public/CoreyLewisResumeVisual.jpg',
-    typeResume: '../../public/CoreyLewisResumeTyped.jpg'
-}
+  music: [],
+  contact: {
+    media: "",
+    text: []
+  },
+  visResume: "../../public/CoreyLewisResumeVisual.jpg",
+  typeResume: "../../public/CoreyLewisResumeTyped.jpg"
+};
