5ad8cc93bbb6806b267f9e8f9943826b84f79798
Corey <lewisc503@gmail.com>
2017-10-10 13:48:41 -0700

    fixed code styling

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,31 +1,37 @@
-import React, {Component} from 'react'
-import TweenMax from 'gsap'
-
-
+import React, { Component } from "react";
+import TweenMax from "gsap";
 
 class Landing extends Component {
+  componentWillEnter(callback) {
+    const box = this.section;
+    TweenMax.fromTo(
+      box,
+      0.5,
+      { bottom: 0, opacity: 0 },
+      { bottom: "50%", opacity: 1, onComplete: callback }
+    );
+  }
 
-    componentWillEnter (callback) {
-      const box = this.section;
-      TweenMax.fromTo(box, 0.5, {bottom: 0, opacity: 0}, {bottom: '50%', opacity: 1, onComplete: callback});
-    }
+  componentWillLeave(callback) {
+    const box = this.section;
+    TweenMax.fromTo(
+      box,
+      0.5,
+      { bottom: "50%", opacity: 1 },
+      { bottom: 0, opacity: 0, onComplete: callback }
+    );
+  }
 
-    componentWillLeave (callback) {
-      const box = this.section;
-      TweenMax.fromTo(box, 0.5, {bottom: '50%', opacity: 1}, {bottom: 0, opacity: 0, onComplete: callback});
-    }
-
-
-    render() {
-        return (
-                <div ref={s => this.section = s}>
-                    <p className="landingHeader">corey lewis</p>
-                    <div className="mainHeader">
-                        <h1>developer</h1>
-                    </div>
-                </div>
-        )
-    }
-} 
+  render() {
+    return (
+      <div ref={s => (this.section = s)}>
+        <p className="landingHeader">corey lewis</p>
+        <div className="mainHeader">
+          <h1>developer</h1>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Landing;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,36 +1,59 @@
-import React, { Component } from 'react'
-import '../styles/main.css'
+import React, { Component } from "react";
+import "../styles/main.css";
 
 class NavBar extends Component {
-
-    render() {
-
-        return (
-            <div>
-                <table className={this.props.data.navBar}>
-                    <tbody>  
-                        <tr>
-                            <td>
-                                <div className={this.props.data.navBtn[0]} onClick={evt => this.props.navClick('about', 0)}>about</div>
-                            </td>
-                            <td>
-                                <div className={this.props.data.navBtn[1]} onClick={evt => this.props.navClick('portfolio', 1)}>portfolio</div>
-                            </td>
-                            <td>
-                                <div className={this.props.data.navBtn[2]} onClick={evt => this.props.navClick('music', 2)}>music</div>
-                            </td>
-                            <td>
-                                <div className={this.props.data.navBtn[3]} onClick={evt => this.props.navClick('resumepage', 3)}>resume</div>
-                            </td>
-                            <td>
-                                <div className={this.props.data.navBtn[4]} onClick={evt => this.props.navClick('contact', 4)}>contact</div>
-                            </td>
-                        </tr>   
-                    </tbody>                
-                </table>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <table className={this.props.data.navBar}>
+          <tbody>
+            <tr>
+              <td>
+                <div
+                  className={this.props.data.navBtn[0]}
+                  onClick={evt => this.props.navClick("about", 0)}
+                >
+                  about
+                </div>
+              </td>
+              <td>
+                <div
+                  className={this.props.data.navBtn[1]}
+                  onClick={evt => this.props.navClick("portfolio", 1)}
+                >
+                  portfolio
+                </div>
+              </td>
+              <td>
+                <div
+                  className={this.props.data.navBtn[2]}
+                  onClick={evt => this.props.navClick("music", 2)}
+                >
+                  music
+                </div>
+              </td>
+              <td>
+                <div
+                  className={this.props.data.navBtn[3]}
+                  onClick={evt => this.props.navClick("resumepage", 3)}
+                >
+                  resume
+                </div>
+              </td>
+              <td>
+                <div
+                  className={this.props.data.navBtn[4]}
+                  onClick={evt => this.props.navClick("contact", 4)}
+                >
+                  contact
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default NavBar;
