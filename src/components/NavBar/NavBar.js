64a66f6cea41181c595ebcdb1864fe2f5092e561
Corey Lewis <lewisc503@gmail.com>
2017-10-13 08:50:32 -0700

    Add react router (#3)
    * wip: react router scaffolding in place
    
    * react router in place

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,56 +1,47 @@
 import React, { Component } from "react";
 import "./navbar.css";
+import stateFile from "../stateFile";
+import { Link } from "react-router-dom";
 
 class NavBar extends Component {
   render() {
     return (
       <div>
-        <table className={this.props.data.navBar}>
-          <tbody>
-            <tr>
-              <td>
-                <div
-                  className={this.props.data.navBtn[0]}
-                  onClick={evt => this.props.navClick("about", 0)}
-                >
-                  about
-                </div>
-              </td>
-              <td>
-                <div
-                  className={this.props.data.navBtn[1]}
-                  onClick={evt => this.props.navClick("portfolio", 1)}
-                >
-                  portfolio
-                </div>
-              </td>
-              <td>
-                <div
-                  className={this.props.data.navBtn[2]}
-                  onClick={evt => this.props.navClick("music", 2)}
-                >
-                  music
-                </div>
-              </td>
-              <td>
-                <div
-                  className={this.props.data.navBtn[3]}
-                  onClick={evt => this.props.navClick("resumepage", 3)}
-                >
-                  resume
-                </div>
-              </td>
-              <td>
-                <div
-                  className={this.props.data.navBtn[4]}
-                  onClick={evt => this.props.navClick("contact", 4)}
-                >
-                  contact
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <Link
+          to="/about"
+          className={stateFile.navBtn[0]}
+          onClick={evt => this.props.navClick("about", 0)}
+        >
+          about
+        </Link>
+        <Link
+          to="/portfolio"
+          className={stateFile.navBtn[1]}
+          onClick={evt => this.props.navClick("portfolio", 1)}
+        >
+          portfolio
+        </Link>
+        <Link
+          to="/music"
+          className={stateFile.navBtn[2]}
+          onClick={evt => this.props.navClick("music", 2)}
+        >
+          music
+        </Link>
+        <Link
+          to="resume"
+          className={stateFile.navBtn[3]}
+          onClick={evt => this.props.navClick("resumepage", 3)}
+        >
+          resume
+        </Link>
+        <Link
+          to="contact"
+          className={stateFile.navBtn[4]}
+          onClick={evt => this.props.navClick("contact", 4)}
+        >
+          contact
+        </Link>
       </div>
     );
   }
