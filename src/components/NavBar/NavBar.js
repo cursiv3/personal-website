0236244d2ace0fd613aac5ba0877d0e95664566a
Corey <lewisc503@gmail.com>
2017-10-13 21:58:01 -0700

    wip: put navbar into drawer

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,26 +1,46 @@
 import React, { Component } from "react";
-import "./navbar.css";
 import { Link } from "react-router-dom";
+import Drawer from "material-ui/Drawer";
+import MenuItem from "material-ui/MenuItem";
+import "./navbar.css";
 
 class NavBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { open: false };
+  }
   render() {
     return (
-      <div>
-        <Link to="/about" className="navBtn">
-          about
-        </Link>
-        <Link to="/portfolio" className="navBtn">
-          portfolio
-        </Link>
-        <Link to="/music" className="navBtn">
-          music
-        </Link>
-        <Link to="resume" className="navBtn">
-          resume
-        </Link>
-        <Link to="contact" className="navBtn">
-          contact
-        </Link>
+      <div className="navEntry">
+        <div onClick={this.handleToggle}>
+          <div className="mobileMenuClicker" />
+          <div className="mobileMenuClicker" />
+          <div className="mobileMenuClicker" />
+        </div>
+        <Drawer
+          docked={false}
+          width={175}
+          open={this.state.open}
+          onRequestChange={open => this.setState({ open })}
+        >
+          <div className="mobileBarCloseBtn" onClick={this.handleClose} />
+          <Link to="/about">
+            <p className="navBtn">about</p>
+          </Link>
+          <Link to="/portfolio">
+            <p className="navBtn">portfolio</p>
+          </Link>
+          <Link to="/music">
+            <p className="navBtn">music</p>
+          </Link>
+          <Link to="/resume">
+            <p className="navBtn">resume</p>
+          </Link>
+          <Link to="/contact">
+            <p className="navBtn">contact</p>
+          </Link>
+        </Drawer>
       </div>
     );
   }
