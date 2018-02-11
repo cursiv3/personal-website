e7cf028ad2f8e1eb2a1ea1cc7b73cf8a8ecf9e0e
Corey <lewisc503@gmail.com>
2018-02-11 12:45:57 -0800

    added media query to each section and increased width to 100% on mobile sizes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,45 +10,55 @@ class About extends Component {
         <div className="aboutSectionWrap">
           <div className="portraitDiv">
             <div className="aboutPortraitBacking" />
-            <img src={data.coreyPortrait} alt={"me"} className="aboutPortrait" />
+            <img
+              src={data.coreyPortrait}
+              alt={"me"}
+              className="aboutPortrait"
+            />
           </div>
-          <h1 className="sectionHead">allow myself to introduce.... myself</h1>
+          <h1 className="aboutSectionHead">
+            allow myself to introduce.... myself
+          </h1>
           <p className="pageText">
-            Hello! My name is Corey, I graduated from Portland State University in
-          2014 with a B.S. in Biological Science. I am a self taught programmer
-          currently working as a Junior Web App Developer with{" "}
+            Hello! My name is Corey, I graduated from Portland State University
+            in 2014 with a B.S. in Biological Science. I am a self taught
+            programmer currently working as a Junior Web App Developer with{" "}
             <a href="https://www.skillsai.com/" target="_blank">
               SkillsAI
-          </a>. I spent the past six months completing an internship with{" "}
+            </a>. I spent the past six months completing an internship with{" "}
             <a href="https://www.transflection.com" target="_blank">
               Transflection
-          </a>, a Portland, OR based digital marketing startup. This was an
-          amazing experience for two reasons: I worked directly under their lead
-          engineer, and this was the first time I'd worked on a real project. We
-          worked with JavaScript, jQuery, Bootstrap, Node, Express/Koa, MongoDB.
-          I finished the internship using ReactJS with Redux.
-        </p>
+            </a>, a Portland, OR based digital marketing startup. This was an
+            amazing experience for two reasons: I worked directly under their
+            lead engineer, and this was the first time I'd worked on a real
+            project. We worked with JavaScript, jQuery, Bootstrap, Node,
+            Express/Koa, MongoDB. I finished the internship using ReactJS with
+            Redux.
+          </p>
           <p className="pageText">
             I have really been enjoying using React and am starting to also work
-          with Angular 5. I built this site with the React library and the repo
-          with the code
-          <a href="https://github.com/cursiv3/personal-website" target="_blank">
+            with Angular 5. I built this site with the React library and the
+            repo with the code
+            <a
+              href="https://github.com/cursiv3/personal-website"
+              target="_blank"
+            >
               {" "}
               can be found here
-          </a>.
-        </p>
+            </a>.
+          </p>
           <p className="pageText">
             I started learning programming in January of 2016 as my search for
-          employment in the field of Biology was not yielding results. A friend
-          recommended "Learn Python the Hard Way" by Zed. A. Shaw. I began to
-          complete coding challenges on sites like HackerRank.com to gain
-          experience using Python, and I ended up dipping my toes in the waters
-          of C++, C#, Java, SQL.
-        </p>
+            employment in the field of Biology was not yielding results. A
+            friend recommended "Learn Python the Hard Way" by Zed. A. Shaw. I
+            began to complete coding challenges on sites like HackerRank.com to
+            gain experience using Python, and I ended up dipping my toes in the
+            waters of C++, C#, Java, SQL.
+          </p>
           <p className="pageText">
             I am also a soccer coach, a certified USSF Soccer referee, guitarist
-          of 17 years and counting, and a video game enthusiast.
-        </p>
+            of 17 years and counting, and a video game enthusiast.
+          </p>
           <div className="aboutImg">
             <img src={data.coreySoccerImg} alt={"me"} />
             <img src={data.coreyChewieImg} alt={"me"} />
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,21 +7,20 @@ class Portfolio extends React.Component {
     return (
       <div className="folioContainer">
         <div className="folioSectionWrap">
-          <h1 className="sectionHead">a few things I've made</h1>
-          <p>
-            {this.props.state.portfolioIntro}
-            <a href="https://github.com/cursiv3/personal-website">
-              on my github.
-          </a>
-          </p>
-          {data.map((d, i) => {
-            let align;
-            (i + 2) % 2 === 0 ? align = 'right' : align = 'left';
+          <div className="folioIntroWrap">
+            <h1 className="folioSectionHead">A few things I've made</h1>
+            <p>
+              {this.props.state.portfolioIntro}
+              <a href="https://github.com/cursiv3/personal-website">
+                on my github.
+              </a>
+            </p>
+          </div>
+          {data.map(d => {
             return (
               <iframe
                 key={d.id}
-                align={align}
-                height='512'
+                height="512"
                 width="50%"
                 scrolling="no"
                 title={d.title}
@@ -29,10 +28,11 @@ class Portfolio extends React.Component {
                 frameBorder="no"
                 allowTransparency="true"
                 allowFullScreen="true"
-                style={{ padding: '10px' }}
-              />)
+                className="folioIframe"
+              />
+            );
           })}
-          <div style={{ clear: 'both' }} />
+          <div style={{ clear: "both" }} />
         </div>
       </div>
     );
