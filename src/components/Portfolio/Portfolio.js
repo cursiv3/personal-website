b66cfc3ed70ba6ddba028b9a71510b207674867c
Corey <lewisc503@gmail.com>
2018-02-19 14:56:09 -0800

    added blurbs to portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -16,20 +16,39 @@ class Portfolio extends React.Component {
               for this sites GitHub repo.
             </p>
           </div>
+          <iframe
+            src="https://codesandbox.io/embed/2oow9n5p7r?view=preview"
+            style={{
+              width: "100%",
+              height: "512px",
+              border: "0",
+              borderRadius: "4px",
+              overflow: "hidden"
+            }}
+            sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+          />
+          <p style={{ textAlign: "center" }}>
+            This is part of a personal project I was working on, simple sign up
+            form with validation and password strength estimation (thanks to the
+            ZXCVBN package).
+          </p>
           {data.map(d => {
             return (
-              <iframe
-                key={d.id}
-                height="512"
-                width="100%"
-                scrolling="no"
-                title={d.title}
-                src={"https:" + d.src}
-                frameBorder="no"
-                allowTransparency="true"
-                allowFullScreen="true"
-                className="folioIframe"
-              />
+              <div>
+                <iframe
+                  key={d.id}
+                  height="512"
+                  width="100%"
+                  scrolling="no"
+                  title={d.title}
+                  src={"https:" + d.src}
+                  frameBorder="no"
+                  allowTransparency="true"
+                  allowFullScreen="true"
+                  className="folioIframe"
+                />
+                <p style={{ textAlign: "center" }}>{d.blurb}</p>
+              </div>
             );
           })}
           <div style={{ clear: "both" }} />
--- src/stateFile/stateFile.js
+++ src/stateFile/stateFile.js
@@ -4,50 +4,41 @@ module.exports = {
       id: 8,
       src:
         "//codepen.io/cursiv3/embed/preview/gvPoVK/?height=265&theme-id=dark&default-tab=result&embed-version=2",
-      title: "FreeCodeCamp Pomodoro Clock"
+      title: "FreeCodeCamp Simon Game",
+      blurb:
+        "This challenge was a good workout using the setTimeout and setInterval methods. Get 20 combos in a row to  win!"
     },
     {
       id: 7,
       src:
         "//codepen.io/cursiv3/embed/preview/LQprWd/?height=265&theme-id=dark&default-tab=result&embed-version=2",
-      title: "FreeCodeCamp Pomodoro Clock"
+      title: "FreeCodeCamp Pomodoro Clock",
+      blurb:
+        "Who knew implementing a simple clock that counts down would be difficult? I decided to use jQuery as I hadn't used it for a really long time."
     },
     {
       id: 6,
       src:
         "//codepen.io/cursiv3/embed/preview/OjQOKx/?height=265&theme-id=dark&default-tab=result&embed-version=2",
-      title: "FreeCodeCamp Weather App"
-    },
-
-    {
-      id: 5,
-      src:
-        "//codepen.io/cursiv3/embed/preview/GERqGP/?height=265&theme-id=dark&default-tab=result&embed-version=2",
-      title: "FreeCodeCamp JS Calculator App"
+      title: "FreeCodeCamp Weather App",
+      blurb:
+        "This challenge gave me a total grasp of callback hell.  I learned the joy that are promises and this knowledge set me up for a lot of future success with consuming REST APIs."
     },
     {
       id: 4,
       src:
         "//codepen.io/cursiv3/embed/preview/xLKJje/?height=265&theme-id=dark&default-tab=result&embed-version=2",
-      title: "Modern Website with React"
+      title: "Modern Website with React",
+      blurb:
+        "I built this in a couple hours to be a simple portfolio addition.  I had seen a bunch of elements and design/styling things on company sites I wanted to learn to do, so I did them here."
     },
     {
       id: 3,
       src:
         "//codepen.io/cursiv3/embed/preview/dWGqYE/?height=265&theme-id=dark&default-tab=result&embed-version=2",
-      title: "Transflection Internship Solo Project"
-    },
-    {
-      id: 2,
-      src:
-        "//codepen.io/cursiv3/embed/preview/PbKPEv/?height=265&theme-id=dark&default-tab=result&embed-version=2",
-      title: "FreeCodeCamp Wikipedia Viewer Challenge"
-    },
-    {
-      id: 1,
-      src:
-        "//codepen.io/cursiv3/embed/preview/qaVQqv/?height=265&theme-id=dark&default-tab=result&embed-version=2",
-      title: "Random Quote Generator Challenge"
+      title: "Transflection Internship Solo Project",
+      blurb:
+        "This was a solo project I did at my internship, migrating a page from HTML/Handlebars to React."
     }
   ],
   about: {
