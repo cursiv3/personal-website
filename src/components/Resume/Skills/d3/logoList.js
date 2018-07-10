0761ba4aee675d537e3505c7d52a2259968d1b35
Corey Lewis <lewisc503@gmail.com>
2018-07-09 21:20:23 -0700

    fixed text on skills barchart

--- src/components/Resume/Skills/d3/logoList.js
+++ src/components/Resume/Skills/d3/logoList.js
@@ -1,50 +1,90 @@
 module.exports = [
-    {
-        name: 'javascript',
-        logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/240px-Unofficial_JavaScript_logo_2.svg.png'
-    },
-    {
-        name: 'html',
-        logo: 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png'
-    },
-    {
-        name: 'css',
-        logo: ''
-    },
-    {
-        name: 'react',
-        logo: ''
-    },
-    {
-        name: 'redux',
-        logo: ''
-    },
-    {
-        name: 'express',
-        logo: ''
-    },
-    {
-        name: 'node',
-        logo: ''
-    },
-    {
-        name: 'postgresql',
-        logo: ''
-    },
-    {
-        name: 'git',
-        logo: ''
-    },
-    {
-        name: 'mongodb',
-        logo: ''
-    },
-    {
-        name: 'aws',
-        logo: ''
-    },
-    {
-        name: 'python',
-        logo: ''
-    }
-]
+  {
+    name: "javascript",
+    level: 13,
+    url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/240px-Unofficial_JavaScript_logo_2.svg.png"
+  },
+  {
+    name: "html",
+    level: 12,
+    url: "https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png"
+  },
+  {
+    name: "css",
+    level: 12,
+    url:
+      "https://i2.wp.com/www.worldeatingdisordersday.org/wp-content/uploads/2016/03/css-logo.png"
+  },
+  {
+    name: "react",
+    level: 12,
+    url:
+      "https://raw.githubusercontent.com/rexxars/react-hexagon/HEAD/logo/react-hexagon.png"
+  },
+  {
+    name: "redux",
+    level: 12,
+    url: "https://s3-us-west-2.amazonaws.com/cslwebsite/reduxLightened.png"
+  },
+  {
+    name: "git",
+    level: 12,
+    url: "http://hamwaves.com/cli/images/git.svg"
+  },
+  {
+    name: "linux",
+    level: 11,
+    url: "http://pngimg.com/uploads/linux/linux_PNG1.png"
+  },
+  {
+    name: "express",
+    level: 9,
+    url:
+      "https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/359/landscape/expressjslogo.png"
+  },
+  {
+    name: "node",
+    level: 9,
+    url: "https://www.brainvire.com/wp-content/uploads/2017/12/nodejs.png"
+  },
+  {
+    name: "postgresql",
+    level: 9,
+    url:
+      "https://cdn.iconscout.com/public/images/icon/free/png-512/postgresql-logo-3116369c4fbec49e-512x512.png"
+  },
+  {
+    name: "docker",
+    level: 8,
+    url:
+      "https://quintagroup.com/cms/technology/Images/docker.png/image_preview"
+  },
+  {
+    name: "mongodb",
+    level: 8,
+    url:
+      "https://static1.squarespace.com/static/513914cde4b0f86e34bbb954/t/58d2c758725e25221a20ed53/1490208601230/mongodb-logo.png"
+  },
+  {
+    name: "aws",
+    level: 8,
+    url: "https://s3-us-west-2.amazonaws.com/cslwebsite/awsLighten.png"
+  },
+  {
+    name: "python",
+    level: 8,
+    url: "https://www.python.org/static/opengraph-icon-200x200.png"
+  },
+  {
+    name: "php",
+    level: 8,
+    url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/2000px-PHP-logo.svg.png"
+  },
+  {
+    name: "graphql",
+    level: 7,
+    url: "https://montykamath.files.wordpress.com/2018/02/graphql.png"
+  }
+];
